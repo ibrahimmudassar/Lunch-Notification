70ba67b13ecff84400a951e0c393d8977c387704
Ibrahim <ibrahimmudassar@gmail.com>
2021-12-01 00:47:42 +0000

    changing lunch companies

--- main.py
+++ main.py
@@ -3,32 +3,27 @@ from datetime import datetime  # For time
 from discord_webhook import DiscordEmbed, DiscordWebhook  # Connect to discord
 from environs import Env  # For environment variables
 from selenium import webdriver  # Browser prereq
-from selenium.common.exceptions import NoSuchElementException  # Exception for selenium
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-
+from selenium.webdriver.support.wait import WebDriverWait
 
 # Setting up environment variables
 env = Env()
 env.read_env()  # read .env file, if it exists
 
 
-# I use opengraph to simplify the collection process
-def embed_to_discord(main, main_desc, alt, alt_desc):
+def embed_to_discord(lunch_list):
     # Webhooks to send to
     webhook = DiscordWebhook(url=env.list("WEBHOOKS"))
 
     # create embed object for webhook
-    embed = DiscordEmbed(title="Lunch for " + datetime.now().strftime("%B %d, %Y"),
-                         description="Today's Lunch Includes:", color="03b2f8")
+    embed = DiscordEmbed(title="Lunch for " +
+                         datetime.now().strftime("%B %d, %Y"), color="03b2f8")
     embed.set_footer(text="By Ibrahim Mudassar")
     embed.set_timestamp()
 
-    embed.add_embed_field(name="Main Entree - " + main,
-                          value=main_desc, inline=False)
-    embed.add_embed_field(name="Alternate - " + alt,
-                          value=alt_desc, inline=False)
+    embed.add_embed_field(name="Today's Lunch Includes:", value="".join(
+        [i + "\n" for i in lunch_list]), inline=False)
 
     # add embed object to webhook(s)
     webhook.add_embed(embed)
@@ -44,29 +39,24 @@ chrome_options.add_argument("--no-sandbox")
 
 browser = webdriver.Chrome(executable_path=env(
     'CHROMEDRIVER_PATH'), options=chrome_options)
-browser.get("https://woodbridge.nutrislice.com/menu/john-f-kennedy-memorial-high-school/lunch/" +
-            datetime.now().strftime("%Y-%m-%d"))
+browser.get("https://www.fdmealplanner.com/#woodbridge")
 
-try:
-    WebDriverWait(browser, 20).until(EC.presence_of_element_located(
-        (By.CSS_SELECTOR, "#content > div > div.splash-container > button"))).click()
-
-except NoSuchElementException:
-    pass
+text_input = WebDriverWait(browser, 20).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, "#location_9"))).click()
 
 
-entree = WebDriverWait(browser, 20).until(EC.presence_of_element_located(
-    (By.CSS_SELECTOR, "#content > ns-menu > div > div.type-container > div > ns-menu-day-loader > div > div > ns-menu-day > div > ns-menu-section > ns-menu-day-section > ul > li:nth-child(1) > ns-menu-station > ul > li > ns-menu-item > div > div > ns-menu-item-food > div > div > a > div > div > span"))).text
+lunch_button = WebDriverWait(browser, 20).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, "#applicationHost > div > div.menuPlanner.notranslate > div > div.page-host > div > div:nth-child(1) > div > div.mealViewerScreen > div.bg-new > div:nth-child(2) > div:nth-child(2) > div > div > div.bg-blk3 > div > div:nth-child(2)"))).click()
 
-entree_description = WebDriverWait(browser, 20).until(EC.presence_of_element_located(
-    (By.CSS_SELECTOR, "#content > ns-menu > div > div.type-container > div > ns-menu-day-loader > div > div > ns-menu-day > div > ns-menu-section > ns-menu-day-section > ul > li:nth-child(1) > ns-menu-station > ul > li > ns-menu-item > div > div > ns-menu-item-food > div > div > a > div > p"))).text
+view_menu = WebDriverWait(browser, 20).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, "#viewmpMenu"))).click()
 
-alternate = WebDriverWait(browser, 20).until(EC.presence_of_element_located(
-    (By.CSS_SELECTOR, "#content > ns-menu > div > div.type-container > div > ns-menu-day-loader > div > div > ns-menu-day > div > ns-menu-section > ns-menu-day-section > ul > li:nth-child(2) > ns-menu-station > ul > li > ns-menu-item > div > div > ns-menu-item-food > div > div > a > div > div > span"))).text
-
-alternate_description = WebDriverWait(browser, 20).until(EC.presence_of_element_located(
-    (By.CSS_SELECTOR, "#content > ns-menu > div > div.type-container > div > ns-menu-day-loader > div > div > ns-menu-day > div > ns-menu-section > ns-menu-day-section > ul > li:nth-child(2) > ns-menu-station > ul > li > ns-menu-item > div > div > ns-menu-item-food > div > div > a > div > p"))).text
-
-embed_to_discord(entree, entree_description, alternate, alternate_description)
+# Tue Nov 30 2021
+date_today = datetime.now().strftime("%a %b %d %Y")
+lunch = WebDriverWait(browser, 20).until(
+    EC.presence_of_element_located((By.NAME, date_today))).text
+lunch = lunch.splitlines()[3:]  # Cleaning up unnecessary lines
 
 browser.quit()
+
+embed_to_discord(lunch)
